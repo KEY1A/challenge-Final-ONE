a9ab81ed2bcd28aa644b3ddd289c9406cfa9118c
Keyla Valdes <valdeskeyla05@gmail.com>
2024-08-11 12:46:00 -0300

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,69 +1,10 @@
-function encriptarTexto(texto) {
-  const reglas = {
-    'e': 'enter',
-    'i': 'imes',
-    'a': 'ai',
-    'o': 'ober',
-    'u': 'ufat'
-  };
-
-  return texto.replace(/[eioua]/g, (letra) => reglas[letra]);
-}
-
-function desencriptarTexto(texto) {
-  const reglas = {
-    'enter': 'e',
-    'imes': 'i',
-    'ai': 'a',
-    'ober': 'o',
-    'ufat': 'u'
-  };
-
-  return texto.replace(/enter|imes|ai|ober|ufat/g, (palabra) => reglas[palabra]);
-}
-
-function copiarTexto() {
-  const textarea = document.getElementById('texto-derecha');
-  const copiarBtn = document.getElementById('copiar-btn');
-  if (textarea.value.trim() === "") {
-    textarea.classList.add('hidden');
-    copiarBtn.classList.add('hidden');
-  } else {
-    textarea.select();
-    navigator.clipboard.writeText(textarea.value)
-      .then(() => {
-        console.log('Texto copiado al portapapeles');
-      })
-      .catch(err => {
-        console.error('Error al copiar el texto: ', err);
-      });
-  }
-}
-
-function escribirTextoLentamente(textarea, texto, intervalo = 40) {
-  let indice = 0;
-  textarea.value = '';
-  const audio = document.getElementById('background-sound');
-  audio.play();
-  
-  const intervalId = setInterval(() => {
-    textarea.value += texto[indice];
-    indice++;
-    if (indice >= texto.length) {
-      clearInterval(intervalId);
-      audio.pause();
-      audio.currentTime = 0; // Reiniciar el audio
-    }
-  }, intervalo);
-}
-
 document.addEventListener('DOMContentLoaded', () => {
   const textareaIzquierda = document.getElementById('texto-izquierda');
   const placeholderImage = document.getElementById('placeholder-image');
   const textareaDerecha = document.getElementById('texto-derecha');
   const copiarBtn = document.getElementById('copiar-btn');
-  const encriptarBtn = document.querySelector('button[type="submit"]');
-  const desencriptarBtn = document.querySelector('button[formaction="desencriptar.php"]');
+  const encriptarBtn = document.getElementById('encriptar-btn');
+  const desencriptarBtn = document.getElementById('desencriptar-btn');
 
   textareaIzquierda.addEventListener('input', () => {
     if (textareaIzquierda.value.trim() !== "") {
@@ -77,16 +18,14 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  encriptarBtn.addEventListener('click', (event) => {
-    event.preventDefault();
+  encriptarBtn.addEventListener('click', () => {
     const texto = textareaIzquierda.value;
     const textoEncriptado = encriptarTexto(texto);
     escribirTextoLentamente(textareaDerecha, textoEncriptado);
     textareaDerecha.classList.remove('hidden');
   });
 
-  desencriptarBtn.addEventListener('click', (event) => {
-    event.preventDefault();
+  desencriptarBtn.addEventListener('click', () => {
     const texto = textareaIzquierda.value;
     const textoDesencriptado = desencriptarTexto(texto);
     escribirTextoLentamente(textareaDerecha, textoDesencriptado);
